d1a5b0b609a7f25ec2bc1d9241484a4cb2491f03
Ivan Voitovych <noctua.vt@gmail.com>
2019-03-14 11:52:20 +0200

    First commit

---
+++ 06_Miscellaneous/JoiningStrings_solution/src/com/example/java8/JoinerMethod.java
@@ -0,0 +1,15 @@
+package com.example.java8;
+
+public class JoinerMethod {
+
+	public static void main(String[] args) {
+
+		String stooges = String.join(" and ", "Larry", "Curly", "Moe");
+		System.out.println(stooges);
+		
+		String[] states = {"California", "Oregon", "Washington"};
+		String statesList = String.join(",", states);
+		System.out.println(statesList);
+		
+	}		
+}
